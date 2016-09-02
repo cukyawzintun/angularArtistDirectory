2265271330dbc44bb58fc78c7d5c61f3e7be8269
Kyaw Zin Tun <kyawzintun@Pauls-iMac.local>
2016-09-02 16:22:55 +0630

    Angular Artist Directory Test App

---
+++ js/app.js
@@ -0,0 +1,19 @@
+var app = angular.module('myApp',[
+		'ngRoute',
+		'artistControllers'
+	]);
+
+app.config(['$routeProvider',function($routeProvider) {
+	$routeProvider.
+	when('/list',{
+		templateUrl: 'partials/list.html',
+		controller:  'ListController'
+	}).
+	when('/details/:itemId', {
+		templateUrl:  'partials/details.html',
+		controller:  'DetailsController'
+	}).
+	otherwise({
+		redirectTo: '/list'
+	});
+}]);
